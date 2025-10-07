3ed21e8a52e2bbb637a4edc14197c1cccc3f44bc
delima-justine <delimajustine24@gmail.com>
2025-10-07 23:50:40 +0800

    add student and its CRUD.

---
+++ php/student.php
@@ -0,0 +1,124 @@
+<?php 
+header('Content-Type: application/json'); // Readable format for JSON in Web.
+// allow 4 methods for CRUD.
+header('Access-Control-Allow-Methods: GET, POST, PATCH, DELETE');
+header("Access-Control-Allow-Origin: *"); // Allows VS Code Go Live to fetch data
+
+$database = "localhost";
+$username = "root";
+$password = "";
+$dbname = "dbenrollment";
+
+// Connect to DB
+$conn = new mysqli($database, $username, $password, $dbname);
+
+// Check connection
+if($conn->connect_error) {
+  die("Connection Failed " . $conn->connect_error);
+} else if($_SERVER['REQUEST_METHOD'] === 'GET') {
+  $response = [];
+
+  $stmt = $conn->prepare("SELECT * FROM tbl_student");
+  $stmt->execute();
+  $result = $stmt->get_result();
+
+  while($row = $result->fetch_assoc()) {
+    array_push($response, array(
+      'student_id'=>$row["student_id"],
+      'student_no'=>$row["student_no"],
+      'last_name'=>$row["last_name"],
+      'first_name'=>$row["first_name"],
+      'email'=>$row["email"],
+      'gender'=>$row["gender"],
+      'year_level'=>$row['year_level'],
+      'program_id'=>$row['program_id']
+    ));
+  }
+
+  echo json_encode($response);
+  $stmt->close();
+} else if($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $student_no = $_POST['student_no'];
+  $last_name = $_POST['last_name'];
+  $first_name = $_POST['first_name'];
+  $email = $_POST['email'];
+  $gender = $_POST['gender'];
+  $year_level = $_POST['year_level'];
+  $program_id = $_POST['program_id'];
+
+  $stmt = $conn->prepare("INSERT INTO 
+                        tbl_student(student_no, last_name, first_name, email, gender,
+                        year_level, program_id )
+                        VALUES(?, ?, ?, ?, ?, ?, ?)");
+
+  $stmt->bind_param("sssssii", 
+    $student_no, $last_name, $first_name, $email, $gender, $year_level,
+      $program_id);
+
+  $stmt->execute();
+
+  if(!$stmt) {
+    die("Insertion error.");
+  }
+
+  echo "Insert success  :D";
+  $stmt->close();
+} else if($_SERVER['REQUEST_METHOD'] === 'PATCH') {
+  parse_str(file_get_contents('php://input'), $_PATCH);
+
+  $student_id = $_PATCH['student_id'] ?? "";
+  $new_student_no = $_PATCH['student_no'] ?? "";
+  $new_last_name = $_PATCH['last_name'] ?? "";
+  $new_first_name = $_PATCH['first_name'] ?? "";
+  $new_email = $_PATCH['email'] ?? "";
+  $new_gender = $_PATCH['gender'] ?? "";
+  $new_year_level = $_PATCH['year_level'] ?? "";
+  $new_program_id = $_PATCH['program_id'] ?? "";
+
+  $stmt = $conn->prepare("
+          UPDATE tbl_student
+          SET student_no = ?,
+          last_name = ?,
+          first_name = ?,
+          email = ?,
+          gender = ?,
+          year_level = ?,
+          program_id = ?
+          WHERE student_id = ?;
+  ");
+
+  $stmt->bind_param("sssssiii", 
+    $new_student_no, 
+    $new_last_name, 
+    $new_first_name, 
+    $new_email, 
+    $new_gender, 
+    $new_year_level,
+    $new_program_id,
+    $student_id
+  );
+
+  $stmt->execute();
+
+  if($stmt->error) {
+    die('There is a problem with the process.');
+  }
+
+  echo "updated successfully.";
+  $stmt->close();
+} else if($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+  parse_str(file_get_contents('php://input'), $_DELETE);
+
+  $student_id = $_DELETE["student_id"] ?? "";
+  $stmt = $conn->prepare("DELETE FROM tbl_student WHERE student_id = ?");
+  $stmt->bind_param('i', $student_id);
+  $stmt->execute();
+
+  if($stmt->error) {
+    die('There is a problem in deleting this one.');
+  }
+
+  echo 'deleted successfully.';
+}
+
+?>
---
+++ scripts/student.js
@@ -0,0 +1,142 @@
+const studentEndpoint = "http://localhost/Enrollment-Management-System/php/student.php"
+
+function displayStudents() {
+  const studTable = document.querySelector("#table_body_student");
+
+  fetch(studentEndpoint)
+  .then((response) => response.json())
+  .then((students)=> {
+    studTable.innerHTML = "";
+
+    for(const student of students) {
+      const row = document.createElement('tr');
+      row.innerHTML = `
+        <td>${student.student_id}</td>
+        <td>${student.student_no}</td>
+        <td>${student.last_name}</td>
+        <td>${student.first_name}</td>
+        <td>${student.email}</td>
+        <td>${student.gender}</td>
+        <td>${student.year_level}</td>
+        <td>${student.program_id}</td>
+        <td><button 
+          type="button"
+          onclick="editStudent(this)">Edit</button></td>
+       <td>
+          <button 
+          type="button"
+          onClick="deleteStudent(this)">
+            Delete
+          </button>
+        </td>
+      `
+      studTable.append(row);
+    }
+  });
+}
+
+function addStudent() {
+  const studentNumInput = document.querySelector('#student_num_input');
+  const lastNameInput = document.querySelector('#last_name_input');
+  const firstNameInput = document.querySelector('#first_name_input'); 
+  const emailInput = document.querySelector('#email_input');
+  const genderInput = document.querySelector('#gender_input');
+  const yearLevelInput = document.querySelector('#year_level_input');
+  const programIdInput = document.querySelector('#program_id_input');
+
+  fetch(studentEndpoint, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: `student_no=${studentNumInput.value}&` +
+          `last_name=${lastNameInput.value}&first_name=${firstNameInput.value}&` +
+          `email=${emailInput.value}&gender=${genderInput.value}&` +
+          `year_level=${yearLevelInput.value}&program_id=${programIdInput.value}`
+  })
+  .then((response) => response.text())
+  .then(responseText => {
+    alert(responseText);
+    displayStudents(); // updates the table
+  }).catch (error => {
+    alert('console error.');
+  })
+}
+
+// Edit Student
+function editStudent(button) {
+  const row = button.closest('tr');
+  const cells = row.querySelectorAll('td');
+  const isEditable = cells[0].contentEditable === "true";
+  const updatedRow = [];
+
+  cells.forEach(cell => {
+    // toggles editable cell.
+    cell.contentEditable = isEditable ? 'false' : 'true';
+  });
+
+  // appends the array.
+  updatedRow.push({
+      student_id: cells[0].innerHTML, 
+      student_no: cells[1].innerHTML,
+      last_name: cells[2].innerHTML,
+      first_name: cells[3].innerHTML,
+      email: cells[4].innerHTML,
+      gender: cells[5].innerHTML,
+      year_level: cells[6].innerHTML,
+      program_id: cells[7].innerHTML,
+  });
+
+  console.log(updatedRow); // prints the updated array.
+
+  if (isEditable) {
+    // Save mode
+    button.textContent = "Edit";
+
+    fetch(studentEndpoint, {
+      method: 'PATCH',
+      headers: {
+        "Content-type": "application/x-www-form-urlencoded",
+      },
+      body: `student_id=${updatedRow[0].student_id}&student_no=${updatedRow[0].student_no}&` +
+          `last_name=${updatedRow[0].last_name}&first_name=${updatedRow[0].first_name}&` +
+          `email=${updatedRow[0].email}&gender=${updatedRow[0].gender}&` +
+          `year_level=${updatedRow[0].year_level}&program_id=${updatedRow[0].program_id}`
+    })
+    .then((response) => response.text())
+    .then((responseText) => {
+      alert(responseText);
+      displayStudents();
+    });
+
+    updatedRow.length = 0; // clear the array.
+  } else {
+    // Edit mode
+    button.textContent = "Save";
+    cells[0].focus();
+    updatedRow.length = 0; // clear the array.
+  }
+}
+
+// Delete Student
+function deleteStudent(button) {
+  const row = button.closest('tr');
+  const cells = row.querySelectorAll('td');
+  const studentId = cells[0].innerHTML;
+  
+  fetch(studentEndpoint, {
+    method: 'DELETE',
+    headers: {
+      "Content-type": "application/x-www-form-urlencoded",
+    },
+    body: `student_id=${studentId}`,
+  })
+  .then((response) => response.text())
+  .then((responseText) => {
+    alert(responseText);
+    displayStudents();
+  })
+}
+
+// Display students
+displayStudents();
