cf47eb76e1c1b9c6960b91f8b4cc0861f806c18d
delima-justine <delimajustine24@gmail.com>
2025-10-09 08:25:18 +0800

    add program api

---
+++ php/program.php
@@ -0,0 +1,105 @@
+<?php 
+header('Content-Type: application/json'); // Readable format for JSON in Web.
+// allow 4 methods for CRUD.
+header('Access-Control-Allow-Methods: GET, POST, PATCH, DELETE');
+header("Access-Control-Allow-Origin: *"); // Allows VS Code Go Live to fetch data
+
+$database = "localhost";
+$username = "root";
+$password = "";
+$dbname = "dbenrollment";
+
+// Connect to DB
+$conn = new mysqli($database, $username, $password, $dbname);
+
+// Check connection
+if($conn->connect_error) {
+  die("Connection Failed " . $conn->connect_error);
+} else if($_SERVER['REQUEST_METHOD'] === 'GET') {
+  $response = [];
+
+  $stmt = $conn->prepare("SELECT * FROM tbl_program");
+  $stmt->execute();
+  $result = $stmt->get_result();
+
+  while($row = $result->fetch_assoc()) {
+    array_push($response, array(
+      'program_id'=>$row["program_id"],
+      'program_code'=>$row["program_code"],
+      'program_name'=>$row["program_name"],
+      'dept_id'=>$row["dept_id"]
+    ));
+  }
+
+  echo json_encode($response);
+  $stmt->close();
+} else if($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $program_code = $_POST['program_code'];
+  $program_name = $_POST['program_name'];
+  $dept_id = $_POST['dept_id'];
+
+  $stmt = $conn->prepare("INSERT INTO 
+                        tbl_program(
+                          program_code, 
+                          program_name, 
+                          dept_id
+                        )
+                        VALUES(?, ?, ?)");
+
+  $stmt->bind_param("ssi", $program_code, $program_name, $dept_id);
+
+  $stmt->execute();
+
+  if(!$stmt) {
+    die("Insertion error.");
+  }
+
+  echo "Insert success";
+  $stmt->close();
+} else if($_SERVER['REQUEST_METHOD'] === 'PATCH') {
+  parse_str(file_get_contents('php://input'), $_PATCH);
+
+  $program_id = $_PATCH['program_id'] ?? "";
+  $new_program_code = $_PATCH['program_code'] ?? "";
+  $new_program_name = $_PATCH['program_name'] ?? "";
+  $new_dept_id = $_PATCH['dept_id'] ?? "";
+
+  $stmt = $conn->prepare("
+          UPDATE tbl_program
+          SET program_code = ?,
+          program_name = ?,
+          dept_id = ?
+          WHERE program_id = ?;
+  ");
+
+  $stmt->bind_param("ssii", 
+    $new_program_code, 
+    $new_program_name, 
+    $new_dept_id,
+    $program_id
+  );
+
+  $stmt->execute();
+
+  if(!$stmt) {
+    die("Prepare failed: (" . $conn->errno . ") " . $conn->error);
+  }
+
+  echo "updated successfully.";
+  $stmt->close();
+} else if($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+  parse_str(file_get_contents('php://input'), $_DELETE);
+
+  $dept_id = $_DELETE['dept_id'] ?? "";
+  $stmt = $conn->prepare("DELETE FROM tbl_program WHERE dept_id = ?");
+  $stmt->bind_param('i', $dept_id);
+  $stmt->execute();
+
+  if(!$stmt) {
+    die('There is a problem in deleting this one.');
+  }
+
+  echo 'deleted successfully.';
+}
+
+?>
